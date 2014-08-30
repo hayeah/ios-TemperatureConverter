d64f6bc0650931c024803e5d43e3d8e45439480a
Howard Yeh <howard@metacircus.com>
2014-08-30 21:11:18 +0800

    reacting to slider changes

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -15,8 +15,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var farenheitLabel: UILabel!
     @IBOutlet weak var temperatureSlider: UISlider!
 
+    @IBAction func sliderChanged(sender: UISlider, forEvent event: UIEvent) {
+        // println("slider.value: \(sender.value)")
+        updateTemperatureDisplay()
+    }
+
+    func updateTemperatureDisplay() {
+        let celsius = temperatureSlider.value
+        let farenheit = celsius * 9 / 5 + 32
+
+        // A local function to help with formatting
+        func formatFloat(f: Float) -> String {
+            return String(format: "%.1f", arguments: [f])
+        }
+
+        celsiusLabel.text = "\(formatFloat(celsius))C"
+        farenheitLabel.text = "\(formatFloat(farenheit))F"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateTemperatureDisplay()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
