cbbdee7271ad9b574023965b2d5200fb71112827
Howard Yeh <howard@metacircus.com>
2014-10-20 16:35:48 +0800

    can convert temperatures

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var kelvinLabel: UILabel!
+    @IBOutlet weak var temperatureSlider: UISlider!
+
+    @IBAction func temperatureSliderChanged(sender: UISlider) {
+        updateTemperatureDisplays()
+    }
+
+//   `F = (C x 9/5) + 32`
+//   `K = C + 273.15`
+    func updateTemperatureDisplays() {
+        let c = temperatureSlider.value
+        let f = c * 9/5 + 32
+        let k = c + 273.15
+        celsiusLabel.text = String(format: "%.1fC", arguments: [c])
+        fahrenheitLabel.text = String(format: "%.1fF", arguments: [f])
+        kelvinLabel.text = String(format: "%.2fK", arguments: [k])
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        temperatureSlider.maximumValue = 100
+        temperatureSlider.minimumValue = 0
+        temperatureSlider.value = 30
+
+        updateTemperatureDisplays()
     }
 
     override func didReceiveMemoryWarning() {
