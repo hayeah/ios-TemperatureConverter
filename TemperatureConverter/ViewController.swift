af1e2c30345fb3f9d6cadbefb33f1ebb035977b5
Howard Yeh <howard@metacircus.com>
2014-10-21 18:05:57 +0800

    adaptive title label with code

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
     @IBOutlet weak var kelvinLabel: UILabel!
@@ -40,11 +41,30 @@ class ViewController: UIViewController {
         updateTemperatureDisplays()
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        println("will layout subviews")
+
+        view.superview
+        let screen = UIScreen.mainScreen()
+        // always return the size of the screen in portraits orientation
+        println("screen fixed bounds: \(screen.fixedCoordinateSpace.bounds)")
+        // since ios8, this reflects the size of the screen in its current orientation
+        println("screen bounds: \(screen.bounds)")
+
+        println("top layout guide: \(topLayoutGuide.length)")
+
+        // center the lable using intrinsic content size
+        // titleLabel.center = CGPoint(x: view.center.x, y: topLayoutGuide.length + 16.0 + titleLabel.frame.size.height / 2)
+
+        // stretch to fit the screen
+        titleLabel.frame = CGRect(x: 16, y: topLayoutGuide.length + 16, width: screen.bounds.width-32, height: 21)
+
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
